c10da90a3d312baea50151c3b6dd1b80aa4fc5c4
aqShanto <abdulquymshanto@gmail.com>
2024-11-16 22:09:13 +0600

    first commit

---
+++ MergingArray.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl "\n"
+double pi = 3.141592653589793238462643383279;
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n), b(m);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for (int i = 0; i < m; i++) {
+        cin >> b[i];
+    }
+    int i = 0, j = 0, k = 0;
+    vector<int> c(n + m);
+    while (i < n || j < m) {
+        if (j >= m || (i < n && a[i] <= b[j])) {
+            c[k] = a[i];
+            k++;
+            i++;
+        } else {
+            c[k] = b[j];
+            k++;
+            j++;
+        }
+    }
+    for (int i = 0; i < m + n; i++) {
+        cout << c[i] << " ";
+    }
+    cout << endl;
+}
+
+signed main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t = 1;
+    // cin>>t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
---
+++ NumberOfSmaller.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl "\n"
+double pi = 3.141592653589793238462643383279;
+
+void solve() {
+}
+
+signed main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t = 1;
+    // cin>>t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
