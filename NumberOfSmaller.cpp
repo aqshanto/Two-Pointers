3ee027f221266075b7946a08cf54a8a65f3a2129
aqShanto <abdulquymshanto@gmail.com>
2024-11-16 22:11:12 +0600

    up

--- NumberOfSmaller.cpp
+++ NumberOfSmaller.cpp
@@ -5,6 +5,15 @@ using namespace std;
 double pi = 3.141592653589793238462643383279;
 
 void solve() {
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n), b(m);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for (int i = 0; i < m; i++) {
+        cin >> b[i];
+    }
 }
 
 signed main() {
